2bf6a19c1504644e74ccd8cef9ab45042fd872f1
andrioluss <56278628+andrioluss@users.noreply.github.com>
2019-11-18 22:23:52 +0100

    Add files via upload

--- model/RemOp.java
+++ model/RemOp.java
@@ -3,11 +3,12 @@ package model;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface RemOp  extends Remote {
-
-    //firma metodo conta righe
-    public int conta_righe(String fileName, int soglia) throws RemoteException;
-
-    //firma metodo elimina riga
-    public int elimina_riga(String fileName, int nRiga) throws RemoteException;
+public interface RemOp extends Remote {
+	
+	//firma metodo conta righe
+	public int conta_righe(String fileName, int soglia) throws RemoteException;
+	
+	//firma metodo elimina riga
+	public int elimina_riga(String fileName, int nRiga) throws RemoteException;
+	
 }
